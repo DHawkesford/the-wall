d0b8b91f0402313bd3ed154cb3dd257299ab9776
danielhawkesford <daniel.hawkesford@gmail.com>
2022-07-21 23:55:11 +0100

    Added auth0 wrapper and login/logout buttons

--- front-end/src/Components/NavBar/index.js
+++ front-end/src/Components/NavBar/index.js
@@ -1,8 +1,15 @@
+import LoginButton from "../LoginButton";
+import LogoutButton from "../LogoutButton";
+import Profile from "../Profile";
+
 const NavBar = ({ handleClick, handleChange, newImageURL }) => {
   return (
     <nav className="navbar">
       <input type="text" className="input" onChange={handleChange} value={newImageURL}/>
       <button className="submit" onClick={handleClick}>submit</button>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
     </nav>
   );
 };
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./Components/App";
 import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-bpskse9l.us.auth0.com"
+      clientId="CUQhOhStf6ezJ7ECMjHuRtW1KjJigfO0"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
