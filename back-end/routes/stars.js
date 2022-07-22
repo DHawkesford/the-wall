a34174b3af924127b80474a46b9290290cb2802b
danielhawkesford <daniel.hawkesford@gmail.com>
2022-07-23 00:51:12 +0100

    Ensured cors is added to postNewVote function

--- back-end/routes/stars.js
+++ back-end/routes/stars.js
@@ -1,6 +1,7 @@
 import express, { query } from "express";
 import { auth, requiredScopes } from 'express-oauth2-jwt-bearer';
 import { getAllStars, getUsersStars, postNewVote } from "../models/stars.js";
+import cors from 'cors';
 
 const router = express.Router();
 
@@ -25,6 +26,7 @@ router.get("/:id", checkJwt, requiredScopes('read:current_user_stars'), async fu
 router.post("/:userID/:imageID", async function(req, res, next) {
   const userID = req.params.userID;
   const imageID = req.params.imageID;
+  cors();
   const data = await postNewVote(userID, imageID);
   res.json({ success: true, payload: data })
 })
