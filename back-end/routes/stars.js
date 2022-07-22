6ee15316c17e6589ae1227a027116c869c380f7c
danielhawkesford <daniel.hawkesford@gmail.com>
2022-07-23 00:58:41 +0100

    Modified enable cors function to app

--- back-end/routes/stars.js
+++ back-end/routes/stars.js
@@ -26,7 +26,6 @@ router.get("/:id", checkJwt, requiredScopes('read:current_user_stars'), async fu
 router.post("/:userID/:imageID", async function(req, res, next) {
   const userID = req.params.userID;
   const imageID = req.params.imageID;
-  cors();
   const data = await postNewVote(userID, imageID);
   res.json({ success: true, payload: data })
 })
