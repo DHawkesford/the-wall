8bdd7ae50ec61ddab1b6425c2e3fbd25db8d25f4
danielhawkesford <daniel.hawkesford@gmail.com>
2022-07-23 10:49:44 +0100

    Attempting new app.use with cors disabled

--- back-end/app.js
+++ back-end/app.js
@@ -15,10 +15,11 @@ app.use((req, res, next) => {
   res.append('Access-Control-Allow-Origin', ['*']);
   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.append('Access-Control-Allow-Headers', 'Content-Type');
+  console.log('response headers set')
   next();
 });
 app.use(logger('dev'));
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
