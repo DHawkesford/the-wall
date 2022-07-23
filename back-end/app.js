85e2704bdf4632ca8789e44485e765b3891b8de9
danielhawkesford <daniel.hawkesford@gmail.com>
2022-07-23 10:52:30 +0100

    Added PATCH to allowed methods

--- back-end/app.js
+++ back-end/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use((req, res, next) => {
   res.append('Access-Control-Allow-Origin', ['*']);
-  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
   res.append('Access-Control-Allow-Headers', 'Content-Type');
   console.log('response headers set')
   next();
