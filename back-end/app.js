8e0fc710bb23100ae073247a55d6b04db943111f
danielhawkesford <daniel.hawkesford@gmail.com>
2022-07-23 10:47:54 +0100

    Attempting new app.use

--- back-end/app.js
+++ back-end/app.js
@@ -11,10 +11,11 @@ import starsRouter  from './routes/stars.js';
 
 const app = express();
 
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next()
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 });
 app.use(logger('dev'));
 app.use(cors());
