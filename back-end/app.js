802c2a038bc846f207eda21efa0e2ab19bd10f63
danielhawkesford <daniel.hawkesford@gmail.com>
2022-07-23 10:44:03 +0100

    Added recommended function from w3

--- back-end/app.js
+++ back-end/app.js
@@ -11,6 +11,11 @@ import starsRouter  from './routes/stars.js';
 
 const app = express();
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+});
 app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
