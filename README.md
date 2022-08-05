fc47176d0f9b94ccdba25ad6d7e88b8d1eefac8b
danielhawkesford <daniel.hawkesford@gmail.com>
2022-08-05 20:23:54 +0100

    Added hamburger icon to NavBar

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 Star Icon by <a href="https://freeicons.io/profile/75801">Hilmy Abiyyu Asad</a> on <a href="https://freeicons.io">freeicons.io</a>
 
 Zoom In Icon by <a href="https://freeicons.io/profile/714">Raj Dev</a> on <a href="https://freeicons.io">freeicons.io</a>
-                                
+
+<a target="_blank" href="https://icons8.com/icon/JTddWDKbAzgl/menu">Menu</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
--- front-end/src/Components/NavBar/index.js
+++ front-end/src/Components/NavBar/index.js
@@ -2,6 +2,7 @@ import LoginButton from "../LoginButton";
 import LogoutButton from "../LogoutButton";
 import Profile from "../Profile";
 import { useAuth0 } from "@auth0/auth0-react";
+import hamburgerIcon from './hamburger_icon.svg';
 
 const NavBar = ({ handleClick, handleChange, newImageURL, usersStars, getUsersStars }) => {
   const { isAuthenticated } = useAuth0();
@@ -27,6 +28,7 @@ const NavBar = ({ handleClick, handleChange, newImageURL, usersStars, getUsersSt
         )
         : null
       }
+      <img src={hamburgerIcon} alt="A hamburger menu icon, which is three horizontal lines in a column." />
     </nav>
   );
 };
