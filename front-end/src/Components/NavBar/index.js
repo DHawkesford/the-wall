12358bd5a051abf2ce2013ec3cfaf53ecb11cc3e
danielhawkesford <daniel.hawkesford@gmail.com>
2022-08-05 20:45:40 +0100

    Added state and styling for toggling the visibility of the hamburger menu

--- front-end/src/Components/NavBar/index.js
+++ front-end/src/Components/NavBar/index.js
@@ -3,32 +3,38 @@ import LogoutButton from "../LogoutButton";
 import Profile from "../Profile";
 import { useAuth0 } from "@auth0/auth0-react";
 import hamburgerIcon from './hamburger_icon.svg';
+import { useState } from 'react';
 
 const NavBar = ({ handleClick, handleChange, newImageURL, usersStars, getUsersStars }) => {
+  const [displayHamburgerMenu, setDisplayHamburgerMenu] = useState(false);
+  const hamburgerMenuClasses = displayHamburgerMenu ? "hamburgerMenu show-hamburger-menu" : "hamburgerMenu hide-hamburger-menu";
+
   const { isAuthenticated } = useAuth0();
 
   return (
     <nav className="navbar">
-      <input type="text" className="input" onChange={handleChange} value={newImageURL}/>
-      <button className="submit" onClick={handleClick}>submit</button>
-      <LoginButton />
-      <LogoutButton />
-      <Profile />
-      {isAuthenticated ? (
-        <div>
-          <button onClick={getUsersStars}>Get your stars</button>
-          <p>
-            {usersStars ? (
-              usersStars.map(star => <>{star}, </>)
-            ) : (
-              "No user's stars defined"
-            )}
-          </p>
-        </div>
-        )
-        : null
-      }
-      <img src={hamburgerIcon} alt="A hamburger menu icon, which is three horizontal lines in a column." />
+      <img src={hamburgerIcon} onClick={() => {setDisplayHamburgerMenu(!displayHamburgerMenu)}} alt="A hamburger menu icon, which is three horizontal lines in a column." />
+      <div className={hamburgerMenuClasses}>
+        <input type="text" className="input" onChange={handleChange} value={newImageURL}/>
+        <button className="submit" onClick={handleClick}>submit</button>
+        <LoginButton />
+        <LogoutButton />
+        <Profile />
+        {isAuthenticated ? (
+          <div>
+            <button onClick={getUsersStars}>Get your stars</button>
+            <p>
+              {usersStars ? (
+                usersStars.map(star => <>{star}, </>)
+              ) : (
+                "No user's stars defined"
+              )}
+            </p>
+          </div>
+          )
+          : null
+        }
+      </div>
     </nav>
   );
 };
