00d24ead3a833408761c014d0b2deb66ae95f152
danielhawkesford <daniel.hawkesford@gmail.com>
2022-08-07 22:51:51 +0100

    Added positioning and styling to upload form modal

--- front-end/src/Components/NavBar/index.js
+++ front-end/src/Components/NavBar/index.js
@@ -3,12 +3,12 @@ import Information from './Information';
 import photographer from './photographer.png';
 import { useState } from 'react';
 
-const NavBar = ({ handleClick, handleChange, newImageURL, usersStars, getUsersStars }) => {
+const NavBar = ({ handleClick, handleChange, newImageURL, usersStars, getUsersStars, setDisplayUploadFormModal }) => {
   const [displayHamburgerMenu, setDisplayHamburgerMenu] = useState(false);
 
   return (
     <nav>
-      <HamburgerMenu displayHamburgerMenu={displayHamburgerMenu} setDisplayHamburgerMenu={setDisplayHamburgerMenu} handleClick={handleClick} handleChange={handleChange} newImageURL={newImageURL} usersStars={usersStars} getUsersStars={getUsersStars} />
+      <HamburgerMenu displayHamburgerMenu={displayHamburgerMenu} setDisplayHamburgerMenu={setDisplayHamburgerMenu} handleClick={handleClick} handleChange={handleChange} newImageURL={newImageURL} usersStars={usersStars} getUsersStars={getUsersStars} setDisplayUploadFormModal={setDisplayUploadFormModal} />
       <p className="theme">
         <span>Today's theme is.. Nature!</span>
         <img src={photographer} className="theme-icon" alt="An illustration of a person with a camera on a strap around their neck." />
