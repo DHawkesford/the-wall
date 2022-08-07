f96829cc3fa64c0819b0dd9ab5bcc3950f18a7e0
danielhawkesford <daniel.hawkesford@gmail.com>
2022-08-07 20:07:01 +0100

    Updated gallery to use smaller scaled image urls from Cloudinary transformation

--- front-end/src/Components/Gallery/GalleryImage/index.js
+++ front-end/src/Components/Gallery/GalleryImage/index.js
@@ -1,10 +1,12 @@
 import zoomIn from './zoom_in_icon.svg';
 
 const GalleryImage = ({ image, star, usersStars, showModal }) => {
+  const smallImageUrl = image.url.slice(0, image.url.indexOf('upload') + 7) + 'c_scale,h_300/' + image.url.slice(image.url.indexOf('upload') + 7);
+
   return( 
     <div className="GalleryImage">
       <img className="zoomIn" src={zoomIn} onClick={() => {showModal(image)}} alt="A magnifying glass with a plus sign over the lens" />
-      <img className="photo" src={image.url} alt={image.alt || 'No alt text available'} />
+      <img className="photo" src={smallImageUrl} alt={image.alt || 'No alt text available'} />
       <div className="starBar">
         <p>Stars: {image.stars}</p>
         {usersStars 
