9b7ae4f53b6e3a14008c75d10c357d3c97a97951
danielhawkesford <daniel.hawkesford@gmail.com>
2022-08-25 15:22:02 +0100

    Added photographer icons and added a randomise function on click

--- front-end/src/Components/NavBar/index.js
+++ front-end/src/Components/NavBar/index.js
@@ -1,17 +1,33 @@
 import HamburgerMenu from './HamburgerMenu';
 import Information from './Information';
-import photographer from './photographer.png';
+import photographer_1 from './photographer_1.png';
+import photographer_2 from './photographer_2.png';
+import photographer_3 from './photographer_3.png';
+import photographer_4 from './photographer_4.png';
+import photographer_5 from './photographer_5.png';
+import photographer_6 from './photographer_6.png';
+import photographer_7 from './photographer_7.png';
+import photographer_8 from './photographer_8.png';
 import { useState } from 'react';
 
 const NavBar = ({ handleClick, handleChange, newImageURL, usersStars, getUsersStars, setDisplayUploadFormModal }) => {
   const [displayHamburgerMenu, setDisplayHamburgerMenu] = useState(false);
+  const photographerIcons = [photographer_1, photographer_2, photographer_3, photographer_4, photographer_5, photographer_6, photographer_7, photographer_8];
+  const [currentPhotographer, setCurrentPhotographer] = useState(photographerIcons[Math.floor(Math.random()*photographerIcons.length)]);
+
+  function randomisePhotographer() {
+    const temp = [...photographerIcons];
+    const indexOfCurrentPhotographer = temp.indexOf(currentPhotographer);
+    temp.splice(indexOfCurrentPhotographer, 1)
+    setCurrentPhotographer(temp[Math.floor(Math.random()*(temp.length))])
+  }
 
   return (
     <nav>
       <HamburgerMenu displayHamburgerMenu={displayHamburgerMenu} setDisplayHamburgerMenu={setDisplayHamburgerMenu} handleClick={handleClick} handleChange={handleChange} newImageURL={newImageURL} usersStars={usersStars} getUsersStars={getUsersStars} setDisplayUploadFormModal={setDisplayUploadFormModal} />
       <p className="theme">
         <span>Today's theme is.. Nature!</span>
-        <img src={photographer} className="theme-icon" alt="An illustration of a person with a camera on a strap around their neck." />
+        <img src={currentPhotographer} className="theme-icon" onClick={randomisePhotographer} alt="An illustration of a person with a camera on a strap around their neck." title="Change the photographer!" />
       </p>
       <Information setDisplayHamburgerMenu={setDisplayHamburgerMenu} />
     </nav>
