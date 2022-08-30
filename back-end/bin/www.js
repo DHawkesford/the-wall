0e896b3ed0b2079bce17f35dc722205c85b02341
danielhawkesford <daniel.hawkesford@gmail.com>
2022-08-30 21:03:00 +0100

    Added test cron job

--- back-end/bin/www.js
+++ back-end/bin/www.js
@@ -8,7 +8,7 @@ import debugLib from "debug";
 import http from "http";
 import { server as webSocketServer } from "websocket";
 import db from "../db/connection.js";
-import cron from 'node-cron';
+// import cron from 'node-cron';
 
 import app from "../app.js";
 
@@ -58,48 +58,48 @@ function originIsAllowed(origin) {
 
 wsServer.on('connect', function(connection) {
 
-  cron.schedule('0-59 * * * *', () => {
-    connection.sendUTF(JSON.stringify({message: 'hello world', star: 'test2'}))
-  });
+  // cron.schedule('0-59 * * * *', () => {
+  //   connection.sendUTF(JSON.stringify({message: 'hello world', star: 'test2'}))
+  // });
 
-  setInterval(async () => {
-    const result = await db.query(`
-    WITH tab AS (SELECT *, 
-      EXTRACT(MINUTES FROM created)::int AS cr, 
-      EXTRACT(MINUTES FROM NOW())::int AS mins,
-      (SELECT count(*)::INT FROM stars WHERE stars.imageid = images.id) AS stars
-    FROM images)
+  // setInterval(async () => {
+  //   const result = await db.query(`
+  //   WITH tab AS (SELECT *, 
+  //     EXTRACT(MINUTES FROM created)::int AS cr, 
+  //     EXTRACT(MINUTES FROM NOW())::int AS mins,
+  //     (SELECT count(*)::INT FROM stars WHERE stars.imageid = images.id) AS stars
+  //   FROM images)
     
-    SELECT *
-      FROM tab
-	    WHERE
-    	    cr BETWEEN (mins - mins % 5) AND (mins- mins % 5 + 4)
-        OR 
-          cr BETWEEN ((mins - mins % 5) + 15) % 60 AND ((mins - mins % 5 + 4) + 15) % 60
-        OR 
-          cr BETWEEN ((mins - mins % 5) + 30) % 60 AND ((mins - mins % 5 + 4) + 30) % 60
-        OR 
-          cr BETWEEN ((mins - mins % 5) + 45) % 60 AND ((mins - mins % 5 + 4) + 45) % 60
-      ORDER BY stars DESC, id DESC;
-    `);
+  //   SELECT *
+  //     FROM tab
+	//     WHERE
+  //   	    cr BETWEEN (mins - mins % 5) AND (mins- mins % 5 + 4)
+  //       OR 
+  //         cr BETWEEN ((mins - mins % 5) + 15) % 60 AND ((mins - mins % 5 + 4) + 15) % 60
+  //       OR 
+  //         cr BETWEEN ((mins - mins % 5) + 30) % 60 AND ((mins - mins % 5 + 4) + 30) % 60
+  //       OR 
+  //         cr BETWEEN ((mins - mins % 5) + 45) % 60 AND ((mins - mins % 5 + 4) + 45) % 60
+  //     ORDER BY stars DESC, id DESC;
+  //   `);
 
-    const sqlString = `
-    WITH tab AS (SELECT *, EXTRACT(MINUTES FROM NOW())::int AS mins FROM themes)
+  //   const sqlString = `
+  //   WITH tab AS (SELECT *, EXTRACT(MINUTES FROM NOW())::int AS mins FROM themes)
       
-    SELECT * 
-      FROM tab
-        WHERE id =
-          CASE
-            WHEN mins BETWEEN 0 AND 4 OR mins BETWEEN 15 AND 19 OR mins BETWEEN 30 AND 34 OR mins BETWEEN 45 AND 49 THEN 1
-            WHEN mins BETWEEN 5 AND 9 OR mins BETWEEN 20 AND 24 OR mins BETWEEN 35 AND 39 OR mins BETWEEN 50 AND 54 THEN 2
-            WHEN mins BETWEEN 10 AND 14 OR mins BETWEEN 25 AND 29 OR mins BETWEEN 40 AND 44 OR mins BETWEEN 55 AND 59 THEN 3
-          END;
-    `;
-    const themeResponse = await db.query(sqlString);
+  //   SELECT * 
+  //     FROM tab
+  //       WHERE id =
+  //         CASE
+  //           WHEN mins BETWEEN 0 AND 4 OR mins BETWEEN 15 AND 19 OR mins BETWEEN 30 AND 34 OR mins BETWEEN 45 AND 49 THEN 1
+  //           WHEN mins BETWEEN 5 AND 9 OR mins BETWEEN 20 AND 24 OR mins BETWEEN 35 AND 39 OR mins BETWEEN 50 AND 54 THEN 2
+  //           WHEN mins BETWEEN 10 AND 14 OR mins BETWEEN 25 AND 29 OR mins BETWEEN 40 AND 44 OR mins BETWEEN 55 AND 59 THEN 3
+  //         END;
+  //   `;
+  //   const themeResponse = await db.query(sqlString);
 
-    const data = result.rows;
-    connection.sendUTF(JSON.stringify({success: true, payload: data, star: 'test'}))
-  }, 5000);
+  //   const data = result.rows;
+  //   connection.sendUTF(JSON.stringify({success: true, payload: data, star: 'test'}))
+  // }, 5000);
 })
 
 wsServer.on('request', function(request) {
--- front-end/src/Components/Socket/index.js
+++ front-end/src/Components/Socket/index.js
@@ -1,5 +1,6 @@
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 
-const webSocket = new W3CWebSocket("https://the-wall-dan-blake.herokuapp.com".replace(/^http/, 'ws'), 'broadcast-protocol');
+// const webSocket = new W3CWebSocket("https://the-wall-dan-blake.herokuapp.com".replace(/^http/, 'ws'), 'broadcast-protocol');
+const webSocket = new W3CWebSocket("http://localhost:3001/".replace(/^http/, 'ws'), 'broadcast-protocol');
 
 export default webSocket;
