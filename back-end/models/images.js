b3ed20007d5e63a0e0b8e408d4b807763124c1eb
danielhawkesford <daniel.hawkesford@gmail.com>
2022-08-31 01:13:37 +0100

    Updated model queries

--- back-end/models/images.js
+++ back-end/models/images.js
@@ -10,24 +10,64 @@ export async function getAllImages() {
 
 export async function getTodaysImages() {
   const result = await db.query(`
-    WITH tab AS (SELECT *, 
-      EXTRACT(MINUTES FROM created)::int AS cr, 
-      EXTRACT(MINUTES FROM NOW())::int AS mins,
+  WITH T3 AS (SELECT * FROM (
+    WITH currentTheme AS (SELECT * FROM (
+        (WITH T1 AS (SELECT *, EXTRACT(MINUTES FROM NOW())::int AS currentMinute FROM themes)
+        
+        SELECT *
+          FROM T1
+          WHERE currentMinute % 6 = id * 2 - 2 OR currentMinute % 6 = id * 2 - 1)
+      ) AS T2)
+  
+    SELECT *, 
+      (SELECT theme FROM currentTheme) AS theme,
+      (SELECT id FROM currentTheme) AS themeId,
+      EXTRACT(MINUTES FROM created)::int AS createdMinute,
       (SELECT count(*)::INT FROM stars WHERE stars.imageid = images.id) AS stars
-    FROM images)
-    
-    SELECT *
-      FROM tab
-	    WHERE
-    	    cr BETWEEN (mins - mins % 5) AND (mins- mins % 5 + 4)
-        OR 
-          cr BETWEEN ((mins - mins % 5) + 15) % 60 AND ((mins - mins % 5 + 4) + 15) % 60
-        OR 
-          cr BETWEEN ((mins - mins % 5) + 30) % 60 AND ((mins - mins % 5 + 4) + 30) % 60
-        OR 
-          cr BETWEEN ((mins - mins % 5) + 45) % 60 AND ((mins - mins % 5 + 4) + 45) % 60
-      ORDER BY stars DESC, id DESC;
+      FROM images 
+  ) AS T4)
+  
+  SELECT * FROM T3
+    WHERE createdMinute % 6 = themeid * 2 - 2 OR createdMinute % 6 = themeid * 2 - 1
+  ORDER BY stars DESC, id DESC;
   `);
+      // WHERE EXTRACT(MINUTES FROM created)::int % 6 = 
+    // WITH T1 AS (SELECT *, 
+      // EXTRACT(MINUTES FROM created)::int AS createdMinute, 
+    //   EXTRACT(MINUTES FROM NOW())::int AS currentMinute,
+    //   (SELECT count(*)::INT FROM stars WHERE stars.imageid = images.id) AS stars
+    // FROM images)
+    
+    // SELECT *
+    //   FROM T1
+	  //   WHERE
+    // 	    cr BETWEEN (mins - mins % 5) AND (mins- mins % 5 + 4)
+    //     OR 
+    //       cr BETWEEN ((mins - mins % 5) + 15) % 60 AND ((mins - mins % 5 + 4) + 15) % 60
+    //     OR 
+    //       cr BETWEEN ((mins - mins % 5) + 30) % 60 AND ((mins - mins % 5 + 4) + 30) % 60
+    //     OR 
+    //       cr BETWEEN ((mins - mins % 5) + 45) % 60 AND ((mins - mins % 5 + 4) + 45) % 60
+    //   ORDER BY stars DESC, id DESC;
+  // const result = await db.query(`
+  //   WITH tab AS (SELECT *, 
+  //     EXTRACT(MINUTES FROM created)::int AS cr, 
+  //     EXTRACT(MINUTES FROM NOW())::int AS mins,
+  //     (SELECT count(*)::INT FROM stars WHERE stars.imageid = images.id) AS stars
+  //   FROM images)
+    
+  //   SELECT *
+  //     FROM tab
+	//     WHERE
+  //   	    cr BETWEEN (mins - mins % 5) AND (mins- mins % 5 + 4)
+  //       OR 
+  //         cr BETWEEN ((mins - mins % 5) + 15) % 60 AND ((mins - mins % 5 + 4) + 15) % 60
+  //       OR 
+  //         cr BETWEEN ((mins - mins % 5) + 30) % 60 AND ((mins - mins % 5 + 4) + 30) % 60
+  //       OR 
+  //         cr BETWEEN ((mins - mins % 5) + 45) % 60 AND ((mins - mins % 5 + 4) + 45) % 60
+  //     ORDER BY stars DESC, id DESC;
+  // `);
   return result.rows;
 }
 
--- back-end/models/themes.js
+++ back-end/models/themes.js
@@ -7,16 +7,11 @@ export async function getAllThemes() {
 
 export async function getTodaysTheme() {
   const sqlString = `
-  WITH tab AS (SELECT *, EXTRACT(MINUTES FROM NOW())::int AS mins FROM themes)
+  WITH T1 AS (SELECT *, EXTRACT(MINUTES FROM NOW())::int AS currentMinute FROM themes)
     
   SELECT * 
-    FROM tab
-      WHERE id =
-        CASE
-          WHEN mins BETWEEN 0 AND 4 OR mins BETWEEN 15 AND 19 OR mins BETWEEN 30 AND 34 OR mins BETWEEN 45 AND 49 THEN 1
-          WHEN mins BETWEEN 5 AND 9 OR mins BETWEEN 20 AND 24 OR mins BETWEEN 35 AND 39 OR mins BETWEEN 50 AND 54 THEN 2
-          WHEN mins BETWEEN 10 AND 14 OR mins BETWEEN 25 AND 29 OR mins BETWEEN 40 AND 44 OR mins BETWEEN 55 AND 59 THEN 3
-        END;
+    FROM T1
+    WHERE currentMinute % 6 = id * 2 - 2 OR currentMinute % 6 = id * 2 - 1
   `;
   const result = await db.query(sqlString);
   return result.rows;
